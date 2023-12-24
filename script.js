9af60ee702e1de3d8279b5696ec5e22cb54cb7c3
chrispynuggetz <133781296+chrispynuggetz@users.noreply.github.com>
2023-12-24 11:48:19 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
         // JavaScript to handle the button click event
         function handleClick() {
+                console.log('in click');
             var giftBox = document.getElementById("giftBox");
             if (giftBox.style.display === "none") {
                 giftBox.style.display = "block";
