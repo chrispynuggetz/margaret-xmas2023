c76a42294278493ad6b3c3b48ad6155a0a138cb2
chrispynuggetz <133781296+chrispynuggetz@users.noreply.github.com>
2023-12-24 11:43:40 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+        // JavaScript to handle the button click event
+        document.getElementById("revealButton").addEventListener("click", function() {
+            var giftBox = document.getElementById("giftBox");
+            if (giftBox.style.display === "none") {
+                giftBox.style.display = "block";
+            }
+        });
